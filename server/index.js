f493c885128d6254f1a6711afcd08ee732b44f38
Andrzej Gorgon <andrzej.gorgon.ag@gmail.com>
2020-02-18 20:08:36 +0100

    Return booked tables for the selected day

--- server/index.js
+++ server/index.js
@@ -11,5 +11,31 @@ app.get('/tables', async (request, response) => {
     response.send(results);
 });
 
+app.get('/tables/available', async (request, response) => {
+    const db = await sqlite.open("./db/database.sqlite");
+    console.log(request.query);
+    const tmpDate = new Date(parseInt(request.query.reservationDate, 10));
+    const reservationDate = `${tmpDate.getFullYear()}-${tmpDate.getMonth() < 9 ? '0' : ''}${(tmpDate.getMonth() + 1)}-${tmpDate.getDate() < 10 ? '0' : ''}${tmpDate.getDate()}`;
+    console.log(reservationDate);
+    const results = await db.all(`SELECT reservation_time, table_id, capacity FROM Reservation LEFT JOIN Tables ON table_id=Tables.id WHERE reservation_time BETWEEN '${reservationDate} 12:00' AND '${reservationDate} 20:00'`);
+    let postResults = {};
+
+    results.forEach(r => {
+        const tmpTime = r.reservation_time.split(" ")[1];
+        if (postResults[tmpTime]) {
+            postResults[tmpTime].push({
+                tableId: r.table_id,
+                capacity: r.capacity
+            })
+        } else {
+            postResults[tmpTime] = [{
+                tableId: r.table_id,
+                capacity: r.capacity
+            }];
+        }
+    });
+    response.send(postResults);
+});
+
 
 app.listen(port);
